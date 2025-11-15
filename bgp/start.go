ef0d777082b4adc645948ece1a89170b81a36aa2
Andre Sencioles <asenci@gmail.com>
2025-11-15 19:24:29 +1100

    fix: StartBgp needs to comlete before other ops

--- bgp/start.go
+++ bgp/start.go
@@ -2,37 +2,23 @@ package bgp
 
 import (
 	"context"
-	"fmt"
-	"sync"
 
-	"github.com/osrg/gobgp/v4/api"
 	"github.com/osrg/gobgp/v4/pkg/server"
 	"github.com/rs/zerolog"
 )
 
-func Start(ctx context.Context, s *server.BgpServer, g *api.Global) error {
+func Start(ctx context.Context, s *server.BgpServer) error {
 	logger := zerolog.Ctx(ctx)
 
 	logger.Debug().Msg("Starting BGP server")
 	defer logger.Debug().Msg("Stopped BGP server")
-
-	wg := &sync.WaitGroup{}
-	wg.Go(func() {
-		s.Serve()
-	})
 
 	context.AfterFunc(ctx, func() {
 		logger.Trace().Str("cause", context.Cause(ctx).Error()).Msg("Stopping BGP server")
 		s.Stop()
 	})
 
-	if err := s.StartBgp(ctx, &api.StartBgpRequest{
-		Global: g,
-	}); err != nil {
-		return fmt.Errorf("error starting BGP: %w", err)
-	}
-
-	wg.Wait()
+	s.Serve()
 
 	return nil
 }
